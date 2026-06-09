e88bc731e17eef07c433848b9e582955f2621f30
Mathias <horvatmathias@gmail.com>
2026-06-09 01:14:20 -0300

    useRef

--- src/componentes/Navbar.jsx
+++ src/componentes/Navbar.jsx
@@ -1,7 +1,15 @@
+import { useRef } from "react"
 function Navbar() {
+    const imputRef = useRef(null)
+    const handleClick = () => {
+        imputRef.current.focus()
+    }
     return (
         <header className='flex justify-between p-3sad'>
-            <input type="text" placeholder="buscar" />
+            <div>
+                <input ref={imputRef} type="text" placeholder="buscar" />
+                <button onClick={handleClick} className="border px-2 py-1">Click to focus</button>
+            </div>
             <h1 className='text-2x1 font-medium'>SuperUY</h1>
 
             <nav className='gap-3 flex'>
--- src/componentes/ProductCard.jsx
+++ src/componentes/ProductCard.jsx
@@ -2,7 +2,7 @@ function ProductCard({title, price, category, src}) {
     return (
         <div className="flex flex-col items-start gap-3">
             <img src={src} alt={title}></img>
-            <p className="mt-3 text-xs text-gray-400">{category}</p>
+            <p className="mt-3 text-xs text-grayh-400">{category}</p>
             <p className="mt-1 text-base text-white">{title}</p>
             <p className="mt-1 text-sm text-amber-400">{price}</p>
         </div>)
