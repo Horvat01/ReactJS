973f30ce63cd0e2b031629045a68dfbea1141d38
Mathias <horvatmathias@gmail.com>
2026-06-17 15:13:28 -0300

    f

--- src/AppRouter/AppRouter.jsx
+++ src/AppRouter/AppRouter.jsx
@@ -2,12 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import CategoryBar from '../componentes/CategoryBar';
 import ProductsContainer from '../componentes/ProductsContainer'; 
 import Navbar from '../componentes/Navbar';
-import ProductsContainerWithlog from '../componentes/ProductsContainerWithlog'; 
+import ProductsContainerWithlog from '../componentes/Productscontainerwithlog'; // 👈 Probamos con la 'c' minúscula que es como suele guardarse
 import ProductDetailContainer from "../componentes/ProductDetailContainer";
 import CartProvider from "../providers/CartProvider";
 import Cart from "../componentes/Cart";
 import Checkout from "../componentes/Checkout";
-import Footer from "../componentes/Footer";
+import Footer from "../componentes/Footer"; // F mayúscula
 
 function AppRouter() {
     return (
@@ -18,7 +18,6 @@ function AppRouter() {
                     <CategoryBar />
                     <Routes>
                         <Route path="/" element={<ProductsContainerWithlog />} />
-                        {/* 3. Corregido el nombre del componente aquí abajo también */}
                         <Route path="/category/:categoryname" element={<ProductsContainer />} /> 
                         <Route path="/product/:id" element={<ProductDetailContainer />} />
                         <Route path="/cart" element={<Cart />} />
@@ -32,4 +31,5 @@ function AppRouter() {
     );
 }
 
+export default AppRouter;
 export default AppRouter;
